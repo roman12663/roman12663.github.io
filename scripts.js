9e6e56c3a1c07b23029169a375db54a8a1b45623
benjammin4dayz <42326027+benjammin4dayz@users.noreply.github.com>
2023-08-11 01:41:42 -0400

    actual finishing touches

--- scripts.js
+++ scripts.js
@@ -1,40 +1,100 @@
+/**
+ * @benjammin4dayz on Discord & GitHub
+ */
 class Site {
-  constructor() {}
-
+  /**
+   * Lazy load when a given element has been visible for a specified amount of time
+   *
+   * @param {string} element - The ID of the element that will trigger the event when observed
+   * @param {number} delay - The delay (in milliseconds) that the element must be observed before causing the event
+   * @param {function} callback - Optional callback function to execute when the element was observed
+   */
   static lazyLoad(element, delay, callback) {
     element = document.getElementById(element);
     if (!element) return console.error("[LazyLoad]: Element not found");
     let timeoutId;
 
+    // Use IntersectionObserver API to trigger an event when the element becomes visible
+    // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
     const observer = new IntersectionObserver((entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           const target = entry.target;
 
+          // Set a timeout to load the element after the specified delay
           timeoutId = setTimeout(() => {
             if (callback) {
               callback();
             } else {
+              // If no callback is provided, fill the 'src' attribute with the data contained in 'data-src'
               const dataSrc = target.getAttribute("data-src");
+              // Append Site._currentURL to satify Twitch Embed requirement for a parent referrer
               if (dataSrc) target.src = dataSrc + Site._currentURL;
             }
+
+            // Stop observing the element and log a message
             observer.unobserve(target);
             console.log("[LazyLoad]: " + target.id);
           }, delay);
         } else {
+          // Cancel the timeout if the element leaves the viewport
           clearTimeout(timeoutId);
         }
       });
     });
 
+    // Start observing the element and log a message
     observer.observe(element);
     console.log(`[LazyLoad]: Waiting to load ${element.id}`);
   }
 
-  static creatorCredit() {
-    alert(
-      "This site was designed and developed by Benjammin4dayz\n© 2023 Some Rights Reserved\n\nContact: @benjammin4dayz on Discord"
+  static updatePagePosOnScroll() {
+    // Update page hash to reflect the active article as the user scrolls
+    // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const target = entry.target;
+            const id = target.getAttribute("id");
+            if (id) {
+              // Use replaceState to update the URL without triggering refresh or navigation
+              // https://developer.mozilla.org/en-US/docs/Web/API/History/replaceState
+              window.history.replaceState(null, null, `#${id}`);
+
+              // Highlight the navigational buttons for the current article
+              const navButtons = document.querySelectorAll(
+                "nav.stickybar > button"
+              );
+              navButtons.forEach((button) => {
+                button.classList.remove("active");
+                if (button.getAttribute("data-target") === id)
+                  button.classList.add("active");
+              });
+            }
+          }
+        });
+      },
+      { threshold: 0.5 }
     );
+
+    const pages = document.querySelectorAll(".page");
+
+    pages.forEach((page) => {
+      observer.observe(page);
+    });
+  }
+
+  static creatorCredit() {
+    const message = [
+      "This site was created with ❤️ for use by Roman12663",
+      "© 2023 Benjammin4dayz. Some Rights Reserved.",
+      "",
+      "Developer Contact:",
+      "@benjammin4dayz on GitHub and Discord",
+      "Website: https://benjammin4dayz.github.io",
+    ].join("\n");
+    alert(message);
   }
 
   static get _currentURL() {
@@ -44,14 +104,13 @@ class Site {
 }
 
 class YouTube {
-  constructor() {}
+  /**
+   * Stackoverflow tech to retrieve and embed the latest YouTube video from a channel by its ID.
+   * (Includes YouTube Shorts!)
+   *
+   * https://stackoverflow.com/questions/18267426/html-auto-embedding-recent-uploaded-videos-from-a-youtube-channel
+   */
   static embedLatestVideo() {
-    /**
-     * src:
-     *    https://stackoverflow.com/questions/18267426/html-auto-embedding-recent-uploaded-videos-from-a-youtube-channel
-     * ref:
-     *    jQuery => Pure JavaScript
-     */
     const channelID = "UCboCEPLD2xFTN8Dp-_8eQdg";
     const reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
 
@@ -68,20 +127,5 @@ class YouTube {
           "https://youtube.com/embed/" + id + "?controls=0&showinfo=0&rel=0";
       })
       .catch((e) => console.log(e));
-  }
-}
-
-class TikTok {
-  constructor() {}
-  static embedUserProfile(element) {
-    element = document.getElementById(element);
-    const url = `https://www.tiktok.com/oembed?url=https://www.tiktok.com/@roman12663`;
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        element.innerHTML = data.html;
-      })
-      .catch((e) => console.error(e));
   }
 }
