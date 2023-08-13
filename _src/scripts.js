7e4621194a340074c35ad59cc074527cf20a70df
benjammin4dayz <42326027+benjammin4dayz@users.noreply.github.com>
2023-08-13 05:11:50 -0400

    feat-fix: 📱 viewport scaling

--- _src/scripts.js
+++ _src/scripts.js
@@ -2,6 +2,13 @@
  * @benjammin4dayz on Discord & GitHub
  */
 class Site {
+  static init() {
+    Site.trueViewHeight();
+    Site.updatePagePosOnScroll();
+    Site.lazyLoad("twitch-embed", 100);
+    Site.lazyLoad("youtube-embed", 100, () => YouTube.embedLatestVideo());
+  }
+
   /**
    * Lazy load when a given element has been visible for a specified amount of time
    *
@@ -85,6 +92,10 @@ class Site {
     });
   }
 
+  static trueViewHeight() {
+    new TrueViewHeight();
+  }
+
   static creatorCredit() {
     const message = [
       "This site was created with ❤️ for use by Roman12663",
@@ -127,5 +138,54 @@ class YouTube {
           "https://youtube.com/embed/" + id + "?controls=0&showinfo=0&rel=0";
       })
       .catch((e) => console.log(e));
+  }
+}
+
+class TrueViewHeight {
+  constructor(override) {
+    const { _say } = TrueViewHeight;
+    this._isMobileDevice
+      ? this._init()
+      : override
+      ? (_say("Enabled"), this._init())
+      : _say("Disabled");
+  }
+
+  _init() {
+    // Set a variable `--vh` which represents the actual height of the viewport
+    document.addEventListener("DOMContentLoaded", this._updateViewHeight);
+    // Update `--vh` after the window is resized
+    this._addResizeListener(this._updateViewHeight);
+  }
+
+  _updateViewHeight() {
+    const vh = `${window.innerHeight}px`;
+    TrueViewHeight._say(`Actual Viewport Height: ${vh}`);
+    document.documentElement.style.setProperty("--vh", vh);
+  }
+
+  _addResizeListener(handler) {
+    TrueViewHeight._say("Waiting for window resize event...");
+    window.addEventListener("resize", this._debounce(handler));
+  }
+
+  _debounce(handler) {
+    const delay = Math.floor(Math.random() * 133, 66);
+    let isBeingResized;
+
+    return () => {
+      clearTimeout(isBeingResized);
+      isBeingResized = setTimeout(handler, delay);
+    };
+  }
+
+  static _say(msg = "") {
+    console.log(`[TrueViewHeight]: ${msg}`);
+  }
+
+  get _isMobileDevice() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
   }
 }
